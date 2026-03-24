aac08614d6815278e31f03fc94c3ef2293ea53a3
Varun-Kelkar <vnkelkar11@gmail.com>
2026-03-24 12:16:06 +0530

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todo-list-app/',
+  base: '/todo-list/',
   plugins: [react(), tailwindcss()],
 })
